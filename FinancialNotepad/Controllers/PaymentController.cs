01aef30fbfd717af5dbddcece77e6920016ee27b
Kirill-Spesivtsev <ksby8819@gmail.com>
2022-11-25 16:06:20 +0300

    Added CRUD controllers for Payments and Categories

---
+++ FinancialNotepad/Controllers/PaymentController.cs
@@ -0,0 +1,71 @@
+﻿using FinancialNotepad.Data;
+using FinancialNotepad.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialNotepad.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PaymentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Payments.Include(t => t.Category);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Transaction/AddOrEdit
+        public IActionResult AddOrEdit(int id = 0)
+        {
+            PopulateCategories();
+            if (id == 0)
+                return View(new Payment());
+            else
+                return View(_context.Payments.Find(id));
+        }
+
+        // POST: Transaction/AddOrEdit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,CategoryId,Amount,Note,Date")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                if (transaction.TransactionId == 0)
+                    _context.Add(transaction);
+                else
+                    _context.Update(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(transaction);
+        }
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Payments == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Transactions'  is null.");
+            }
+            var transaction = await _context.Payments.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Payments.Remove(transaction);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
