779b886525a18e16a199493c0389f38a7839bbd7
Kirill-Spesivtsev <ksby8819@gmail.com>
2022-12-19 00:04:58 +0300

    Added Profit plot

--- FinancialNotepad/Controllers/StatisticsController.cs
+++ FinancialNotepad/Controllers/StatisticsController.cs
@@ -30,17 +30,17 @@ namespace FinancialNotepad.Controllers
             double totalIncome = SelectedTransactions
                 .Where(i => i.Type == "Income")
                 .Sum(j => j.Amount);
-            ViewBag.TotalIncome = totalIncome.ToString("C0");
+            ViewBag.TotalIncome = totalIncome.ToString("F") + " $";
 
             double totalExpense = SelectedTransactions
                 .Where(i => i.Type == "Expense")
                 .Sum(j => j.Amount);
-            ViewBag.TotalExpense = totalExpense.ToString("C0");
+            ViewBag.TotalExpense = totalExpense.ToString("F") + " $";
 
             var profit = totalIncome - totalExpense;
             CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
             culture.NumberFormat.CurrencyNegativePattern = 1;
-            ViewBag.Balance = String.Format(culture, "{0:C0}", profit);
+            ViewBag.Balance = String.Format(culture, "{0:F}", profit);
 
             ViewBag.DoughnutChartData = SelectedTransactions
                 .Where(i => i.Type == "Expense")
@@ -49,15 +49,48 @@ namespace FinancialNotepad.Controllers
                 {
                     categoryTitleWithIcon = k.First()?.Category?.Icon + " " + k.First()?.Category?.Title,
                     amount = k.Sum(j => j.Amount),
-                    formattedAmount = k.Sum(j => j.Amount).ToString("C0"),
+                    formattedAmount = k.Sum(j => j.Amount).ToString("F") + " $",
                 })
                 .OrderByDescending(l => l.amount)
                 .ToList();
 
+
+            List<PlotData> incomeStatistics = SelectedTransactions
+                .Where(i => i.Type == "Income")
+                .GroupBy(j => j.Date)
+                .Select(k => new PlotData()
+                {
+                    day = k.First().Date.ToString("dd-MM-yyyy"),
+                    income = k.Sum(l => l.Amount)
+                })
+                .ToList();
+
+            List<PlotData> expenseStatistics = SelectedTransactions
+                .Where(i => i.Type == "Expense")
+                .GroupBy(j => j.Date)
+                .Select(k => new PlotData()
+                {
+                    day = k.First().Date.ToString("dd-MM-yyyy"),
+                    expense = k.Sum(l => l.Amount)
+                })
+                .ToList();
+
             string[] LastWeekData = Enumerable.Range(0, 7)
-                .Select(i => StartDate.AddDays(i).ToString("dd-MMM"))
+                .Select(i => StartDate.AddDays(i).ToString("dd-MM-yyyy"))
                 .ToArray();
 
+            ViewBag.SplineChartData = 
+                    from day in LastWeekData
+                    join income in incomeStatistics on day equals income.day into dayIncomeJoined
+                    from income in dayIncomeJoined.DefaultIfEmpty()
+                    join expense in expenseStatistics on day equals expense.day into expenseJoined
+                    from expense in expenseJoined.DefaultIfEmpty()
+                    select new
+                    {
+                        day = day,
+                        income = income == null ? 0 : income.income,
+                        expense = expense == null ? 0 : expense.expense,
+                    };
 
             ViewBag.RecentTransactions = await _context.Transactions
                 .Include(i => i.Category)
@@ -70,4 +103,11 @@ namespace FinancialNotepad.Controllers
         }
     }
 
+    public class PlotData
+    {
+        public string day;
+        public double income;
+        public double expense;
+
+    }
 }
