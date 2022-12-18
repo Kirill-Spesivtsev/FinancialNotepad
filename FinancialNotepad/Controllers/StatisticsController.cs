b41d3b043f99b0ecf385a158dad8412050077f98
Kirill-Spesivtsev <ksby8819@gmail.com>
2022-12-18 15:20:27 +0300

    Added current budget bars, views improvements

--- FinancialNotepad/Controllers/StatisticsController.cs
+++ FinancialNotepad/Controllers/StatisticsController.cs
@@ -1,5 +1,8 @@
-﻿using FinancialNotepad.Data;
+﻿using System.Globalization;
+using FinancialNotepad.Data;
+using FinancialNotepad.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinancialNotepad.Controllers
 {
@@ -13,9 +16,47 @@ namespace FinancialNotepad.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<ActionResult> Index()
         {
+
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= StartDate && y.Date <= EndDate)
+                .ToListAsync();
+
+            double totalIncome = SelectedTransactions
+                .Where(i => i.Type == "Income")
+                .Sum(j => j.Amount);
+            ViewBag.TotalIncome = totalIncome.ToString("C0");
+
+            double totalExpense = SelectedTransactions
+                .Where(i => i.Type == "Expense")
+                .Sum(j => j.Amount);
+            ViewBag.TotalExpense = totalExpense.ToString("C0");
+
+            var profit = totalIncome - totalExpense;
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+            culture.NumberFormat.CurrencyNegativePattern = 1;
+            ViewBag.Balance = String.Format(culture, "{0:C0}", profit);
+
+            ViewBag.DoughnutChartData = SelectedTransactions
+                .Where(i => i.Type == "Expense")
+                .GroupBy(j => j.Category.CategoryId)
+                .Select(k => new
+                {
+                    categoryTitleWithIcon = k.First()?.Category?.Icon + " " + k.First()?.Category?.Title,
+                    amount = k.Sum(j => j.Amount),
+                    formattedAmount = k.Sum(j => j.Amount).ToString("C0"),
+                })
+                .OrderByDescending(l => l.amount)
+                .ToList();
+
+
             return View();
         }
     }
+
 }
